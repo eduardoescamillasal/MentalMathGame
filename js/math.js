8f8e5c4498369eade9e4d262b47e88c970389dce
Eduardo Escamilla <eduardo.escamillasal@gmail.com>
2021-02-10 19:08:55 -0600

    Added conspiracies on left panel

--- js/math.js
+++ js/math.js
@@ -12,6 +12,9 @@ const category = document.getElementById("category");
 const timeData = document.querySelector('[data-time=timer]');
 const levelData = document.querySelector('[data-level=level]');
 const userStat = document.querySelector('.statList');
+const leftPanel = document.querySelector('.left');
+const conspTitle = document.querySelector('.conspTitle');
+const conspContent = document.querySelector('.conspContent');
 
 
 /***** STATE VARIABLES *****/
@@ -68,6 +71,7 @@ form.onsubmit = function(e) {
 
 stopButton.onclick = function() {
 	const len = pics.length;
+	const docLen = conspiracies.length;
 	document.body.style.backgroundImage= pics[Math.floor(Math.random() * len)];
 	var resultString;
 	var categoryString;
@@ -99,6 +103,10 @@ stopButton.onclick = function() {
 	response.innerHTML = ""; // clear response in case it was set
 	results.innerHTML = resultString;
 	category.innerHTML = categoryString;
+  let index_cons	= Math.floor(Math.random() * docLen);
+	let chosenConspiracy = conspiracies[index_cons];
+	conspTitle.innerText = chosenConspiracy.title;
+	conspContent.innerText = chosenConspiracy.content;
 };
 
 /***** FUNCTIONS ******/
@@ -179,3 +187,27 @@ var getCategory = function(mean,maxi) {
 };
 
 //stopButton.addEventListener('mouseenter', stopButton.onclick)
+
+var mkultra = `MKUltra
+Genuine American research in the 1950s and 1960s into chemical interrogation and mind-control techniques were followed by many conspiracy theories (like Project Monarch), especially following CIA Director Richard Helm's 1973 order to destroy all files related to the project. These theories include the allegation that the mass fatality at Jonestown in 1978 was connected to an MKUltra experiment.`;
+
+var techSupression = `Numerous theories pertain to the alleged suppression of certain technologies and energies. Such theories may focus on the Vril Society Conspiracy, allegations of the suppression of the electric car by fossil-fuel companies (as detailed in the 2006 documentary Who Killed the Electric Car?), and the Phoebus cartel, set up in 1924, which has been accused of suppressing longer-lasting light bulbs.[281] Other long-standing allegations include the suppression of perpetual motion and cold fusion technology by government agencies, special interest groups, or fraudulent inventors.[282]
+
+Promoters of alternative energy theories have included Thomas Henry Moray, Eugene Mallove, and convicted American fraudster Stanley Meyer`;
+
+var falseHistory =`Some theories claim that the dates of historical events have been deliberately distorted. These include the phantom time hypothesis of German conspiracy theorist[citation needed][original research?] Heribert Illig, who in 1991 published an allegation that 297 years had been added to the calendar by establishment figures such as Pope Sylvester II in order to position themselves at the millennium.[292]
+
+A comparable theory, known as the New Chronology, is most closely associated with Russian theorist Anatoly Fomenko. Fomenko holds that history is many centuries shorter than is widely believed and that numerous historical documents have been fabricated, and legitimate documents destroyed, for political ends. Adherents of such ideas have included chess grandmaster Garry Kasparov.`;
+
+var vaccines = `It is claimed that the pharmaceutical industry has mounted a cover-up of a causal link between vaccines and autism. The conspiracy theory developed after the publication in Britain in 1998 of a fraudulent paper by discredited former doctor Andrew Wakefield.[255] The resulting anti-vaccine movement has been promoted by a number of prominent persons including Rob Schneider,[256] Jim Carrey[257] and U.S. President Donald Trump,[258][259] and has led to increased rates of infection and death from diseases such as measles in many countries, including the US, Italy, Germany, Romania and the UK.[260][261][262][263]
+
+Vaccine conspiracy theories have been widespread in Nigeria since at least 2003, as well as in Pakistan. Such theories may feature claims that vaccines are part of a secret anti-Islam plot, and have been linked to fatal mass shootings and bombings at vaccine clinics in both countries.`;
+
+var globWarm = `A global warming conspiracy theory typically alleges that the science behind global warming has been invented or distorted for ideological or financial reasons.[12] Many have promoted such theories, including U.S. President Donald Trump,[267][268] US Senator James Inhofe,[269] British journalist Christopher Booker,[269] and Viscount Christopher Monckton`;
+var conspiracies = [
+	{title:'MKultra', content:mkultra},
+	{title:'Technology Supression', content:techSupression},
+	{title: 'False History', content:falseHistory},
+	{title: 'Vaccines', content:vaccines},
+	{title: 'Global Warming', content:globWarm}
+];
